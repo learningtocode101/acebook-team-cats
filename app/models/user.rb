6290bcca4d21a5f88b73d015a6840da9df4caf01
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-02-08 11:12:59 +0000

    Feature: custom sign up messages implemeneted

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
   validates_presence_of :first_name, :message => "Meow, what is your a first name?"
   validates_presence_of :last_name, :message => "Meow, what is your last name?"
   validates_presence_of :birthday, :message => "Meow, when is your birthday?"
-  validates_presence_of :password, :message => "Meow, what is your password?"
+  validates_presence_of :password
   validates :password, length: { in: 6..10 }
+  validates_presence_of :password, :message => "Meow, what is your password?"
   validates_presence_of :gender, :message => "Meow, what is your gender?"
   validates_presence_of :email, :message => "Meow, what is your email?"
   validates_format_of :email, :with => /.*@.*/, :message => "Meow, email must include @."
